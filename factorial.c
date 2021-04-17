e1c90dca95523dd9a241dba864948050de34ea86
johnanthonyjose <john.anthony.jose@dlsu.edu.ph>
2021-04-17 08:47:21 +0800

    New code to merge

--- factorial.c
+++ factorial.c
@@ -7,8 +7,12 @@
  * @return calculated result of n!
  */
 int factorial (int n) {
-    // ADD CODE HERE
-    return 0; // EDIT THIS
+    if (n > 1){
+        return n * factorial(n -1);
+    }
+    else {
+        return 1;
+    }
 }
 
 
